77e76ed314f5b0ba8c253b479ed99dd0c64dfda2
Krasava237 <glebparamonov76422@gmail.com>
2025-04-17 21:02:46 +0300

    Fix import path for src.main using sys.path hack

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,9 +1,5 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
 from fastapi.testclient import TestClient
-from main import app
+from src.main import app
 
 client = TestClient(app)
 
